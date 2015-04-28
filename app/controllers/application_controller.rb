cd98e7635632dd520b513a06de10d718707799bd
David Feimer <davidfeimer@gmail.com>
2015-04-28 15:52:42 +0000

    up and working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def restricted_access
+  def restrict_access
     if !current_user
       flash[:alert] = "You must log in."
       redirect_to new_session_path
