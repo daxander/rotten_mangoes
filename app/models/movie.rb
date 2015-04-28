eb2a7ef267c5431234d692763352e37ced66d4f7
David Feimer <davidfeimer@gmail.com>
2015-04-28 20:10:03 +0000

    styling

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -19,7 +19,7 @@ class Movie < ActiveRecord::Base
   validates :release_date,
     presence: true
 
-  validate :release_date_is_in_the_future
+  # validate :release_date_is_in_the_future
 
   def review_average
     reviews.sum(:rating_out_of_ten)/reviews.size
@@ -27,10 +27,10 @@ class Movie < ActiveRecord::Base
 
   protected
 
-  def release_date_is_in_the_future
-    if release_date.present?
-      errors.add(:release_date, "should probably be in the future") if release_date < Date.today
-    end
-  end
+  # def release_date_is_in_the_future
+  #   if release_date.present?
+  #     errors.add(:release_date, "should probably be in the future") if release_date > Date.today
+  #   end
+  # end
 
 end
