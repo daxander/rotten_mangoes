71491feeeacec471f4bde9446878c75294f7cb6e
David Feimer <davidfeimer@gmail.com>
2015-04-28 00:53:20 +0000

    review model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,31 +1,32 @@
 class Movie < ActiveRecord::Base
-      
-      validates :title,
-        presence: true
+  has_many :reviews
+  
+  validates :title,
+    presence: true
 
-      validates :director,
-        presence: true
+  validates :director,
+    presence: true
 
-      validates :runtime_in_minutes,
-        numericality: { only_integer: true }
+  validates :runtime_in_minutes,
+    numericality: { only_integer: true }
 
-      validates :description,
-        presence: true
+  validates :description,
+    presence: true
 
-      validates :poster_image_url,
-        presence: true
+  validates :poster_image_url,
+    presence: true
 
-      validates :release_date,
-        presence: true
+  validates :release_date,
+    presence: true
 
-      validate :release_date_is_in_the_future
+  validate :release_date_is_in_the_future
 
-      protected
+  protected
 
-      def release_date_is_in_the_future
-        if release_date.present?
-          errors.add(:release_date, "should probably be in the future") if release_date < Date.today
-        end
-      end
+  def release_date_is_in_the_future
+    if release_date.present?
+      errors.add(:release_date, "should probably be in the future") if release_date < Date.today
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :movies
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   root to: 'movies#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
