0061118fb2c49050bc0f2468128596d494d47c36
David Feimer <davidfeimer@gmail.com>
2015-04-29 19:28:11 +0000

    halfway through admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
 
-  get 'sessions/create'
-
-  get 'users/new'
-
-  get 'users/create'
 
   resources :movies do
     resources :reviews, only: [:new, :create]
   end
-  resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :users
+
+  namespace :admin do
+    resources :users
+  end
+  
   resources :sessions, only: [:new, :create, :destroy]
   root to: 'movies#index'
 
